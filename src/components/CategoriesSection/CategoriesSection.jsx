c55b02add70f88d22a70e3b8db9748dae06e0c53
leon <nashe@god.com>
2025-04-02 01:17:34 -0300

    ULTIMO FIX???????????

--- src/components/CategoriesSection/CategoriesSection.jsx
+++ src/components/CategoriesSection/CategoriesSection.jsx
@@ -1,11 +1,12 @@
 import { CategoriesCard } from './CategoriesCard'
-
+import remeraEjemplo   from '../../assets/remera-ejemplo.jpg'
+import pantalonEjemplo from '../../assets/pantalon-ejemplo.jpg'
 
 export function CategoriesSection() {
   return (
     <section className="categories my-[2rem] flex flex-col max-w-[1024px] mx-auto md:flex-row justify-center items-center gap-4  text-[var(--color-texto)] ">
-      <CategoriesCard text={"Remeras"}    img={'/src/assets/remera-ejemplo.jpg'}/>
-      <CategoriesCard text={"Pantalones"} img={'/src/assets/pantalon-ejemplo.jpg'}/>
+      <CategoriesCard text={"Remeras"} img={remeraEjemplo}/>
+      <CategoriesCard text={"Pantalones"} img={pantalonEjemplo}/>
     </section>
   )
 }
