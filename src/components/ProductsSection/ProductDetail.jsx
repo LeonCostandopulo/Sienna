d1338b6327cc785dccd7b5722ffb46f72032d63f
leon <nashe@god.com>
2025-04-09 23:35:11 -0300

    test img

--- src/components/ProductsSection/ProductDetail.jsx
+++ src/components/ProductsSection/ProductDetail.jsx
@@ -54,7 +54,7 @@ export function ProductDetail({ product, onClose }) {
                   transition: 'transform 0.3s ease-in-out, scale 0.3s ease-in-out',
                   scale: selectedColor === 'Negro' ? '' : '0.7  ',
                 }}
-                src={selectedColor !== 'Negro' ? '' + product.imageUrl.replace("-negro.webp", `-${selectedColor}.webp`) : product.imageUrl}
+                src={product.imageUrl.replace("-negro.webp", `-${selectedColor}.webp`)}
                 alt={product.name}
                 className="product-image scale-120  w-full h-full object-contain"
               />
